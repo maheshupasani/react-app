62745257e8b8e4bccf5825a5c411dd9be51108d0
mahesh <mahesh@ubuntu.ubuntu-domain>
2019-07-13 23:30:15 +0530

    home-cart page commit with routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
+import { Switch,Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
+// importing components
+// Layout (Header-Footer)
+import HeaderComponent from './components/Layout/HeaderComponent';
+// Home Page
+import HomeComponent from './components/Home/HomeComponent';
+// Cart Page
+import CartComponent from './components/Cart/CartComponent';
+// 404 page
+import PageNotFoundComponent from './components/Default/PageNotFoundComponent';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  	return (
+		<React.Fragment>
+			<HeaderComponent/>
+			<Switch>
+				<Route exact path="/" component={ HomeComponent } />
+				<Route path="/cart" component={ CartComponent } />
+				<Route component={ PageNotFoundComponent } />
+			</Switch>
+		</React.Fragment>
+	);
 }
 
 export default App;
---
+++ src/components/Cart/CartComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CartComponent extends Component {
+    render() {
+        return (
+            <div>
+                sdfa
+            </div>
+        )
+    }
+}
